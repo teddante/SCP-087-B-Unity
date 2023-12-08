87542a17c20ba97b48d8c0a350388406861ba75e
Ted Enston <37670093+teddante@users.noreply.github.com>
2023-12-08 16:40:51 +0000

    Add font loading functionality to Game.cs

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     static bool InvertMouse;
+    static Font font1;
+    static Font font;
 
     void Awake()
     {
@@ -33,15 +35,19 @@ public class Game : MonoBehaviour
         Cursor.visible = false;
 
         //Back buffer maybe unnecessary
+
+        font1 = LoadFont("GFX/Courier.ttf", 18);
+        font = LoadFont("GFX/pretext.ttf", 128);
+
     }
 
     /// <summary>
-    /// Sets the screen resolution and fullscreen mode for 3D graphics.
+    /// Sets the screen resolution and full screen mode for 3D graphics.
     /// </summary>
     /// <param name="screenWidth">The width of the screen resolution.</param>
     /// <param name="screenHeight">The height of the screen resolution.</param>
-    /// <param name="colourDepth">The color depth of the graphics (default is 32).</param>
-    /// <param name="FullscreenMode">The fullscreen mode (0 for exclusive fullscreen, 2 for windowed).</param>
+    /// <param name="colourDepth">The colour depth of the graphics (default is 32).</param>
+    /// <param name="FullscreenMode">The full screen mode (0 for exclusive full screen, 2 for windowed).</param>
     void Graphics3D(int screenWidth, int screenHeight, int colourDepth = 32, int FullscreenMode = 0)
     {
         switch (FullscreenMode)
@@ -56,6 +62,33 @@ public class Game : MonoBehaviour
                 Screen.SetResolution(screenWidth, screenHeight, FullScreenMode.ExclusiveFullScreen);
                 break;
         }
+    }
+
+    /// <summary>
+    /// Loads a font asset from the Resources folder.
+    /// </summary>
+    /// <param name="fontName">The name of the font asset to load.</param>
+    /// <param name="height">The height of the font in pixels. Default is 12.</param>
+    /// <param name="bold">Specifies whether the font should be bold. Default is false.</param>
+    /// <param name="italic">Specifies whether the font should be italic. Default is false.</param>
+    /// <param name="underlined">Specifies whether the font should be underlined. Default is false.</param>
+    /// <returns>The loaded font asset, or null if the font is not found.</returns>
+    public static Font LoadFont(string fontName, int height = 12, bool bold = false, bool italic = false, bool underlined = false)
+    {
+        // Load the font from the Resources folder
+        Font font = Resources.Load<Font>(fontName);
+
+        if (font == null)
+        {
+            Debug.LogError("Font not found: " + fontName);
+            return null;
+        }
+
+        // Unity does not natively support changing a font to bold/italic/underlined dynamically.
+        // Usually, you would have a separate font asset for bold/italic versions.
+        // You can add additional logic here to load those versions if needed.
+
+        return font;
     }
 
     void Update()
